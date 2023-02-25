60c801ffda40052bc73a7126548f6e18fd9d13be
mahabub <mkhan@me.iitr.ac.in>
2023-02-26 01:13:03 +0530

    wip

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config(); //to use .env file data
 
 const corsOptions = {
-  origin: "http://localhost:8080",
+  origin: "https://myappmmk.netlify.app/",
   credentials: true,
 };
 
